5e2a6d312757cbfa8ae8ac00bfa39584ce4970f4
mehwishm691 <139951532+mehwishm691@users.noreply.github.com>
2024-02-12 11:37:07 +0500

    new quran
    chang in new quran

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add this to a new script.js file or an existing one
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Add any JavaScript functionality related to the social media icons here
+  });
+  
